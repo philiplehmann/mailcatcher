552b511d3f6a162ce8023635d4a526b7f84f4965
Philip Lehmann <philiplehmann@gmail.com>
2022-06-14 21:19:02 +0200

    add retag of manifest

--- buildx.sh
+++ buildx.sh
@@ -4,6 +4,4 @@ docker buildx create --platform linux/arm64,linux/amd64,linux/ppc64le,linux/s390
 docker buildx use mailcatcher-builder
 docker buildx build --push --no-cache --platform=linux/arm64,linux/amd64,linux/ppc64le,linux/s390x,linux/arm/v7,linux/arm/v6 --tag=philiplehmann/mailcatcher:v0.8.1 .
 
-docker pull philiplehmann/mailcatcher:v0.8.1
-docker tag philiplehmann/mailcatcher:v0.8.1 philiplehmann/mailcatcher:latest
-docker push philiplehmann/mailcatcher:latest
+docker buildx imagetools create registry.hub.docker.com/philiplehmann/mailcatcher:v0.8.1 --tag registry.hub.docker.com/philiplehmann/mailcatcher:latest
